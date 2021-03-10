d58690f6a9781926b5bf22ade5b6a62802d856ed
Diego Xavier Tapia Inchauste <a2018118218@estudiantes.upsa.edu.bo>
2021-03-10 13:01:24 -0400

    Inicio programa 06.03

---
+++ persona/APIpersona/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIpersona.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ persona/APIpersona/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace APIpersona.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
